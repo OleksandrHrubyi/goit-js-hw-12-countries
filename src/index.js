a8f0a56e2a73bb8fbe9b93da2f3ec2a5351b9b8b
OleksandrGrubyi <ghrubyi@ukr.net>
2020-11-15 01:43:44 +0200

    add right search

--- src/index.js
+++ src/index.js
@@ -1,43 +1,115 @@
 import './styles.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css'
+import markup from '../src/handlTamplate/main.hbs';
+import remarkup from '../src/handlTamplate/list.hbs';
+
 const lodash = require('lodash');
-
-import murkup from '../src/handlTamplate/main.hbs';
-
-
-
+const {error} = require('@pnotify/core')
+const mainHref = 'https://restcountries.eu/rest';
 const ref = {
-    main: document.querySelector('.main'),
-    input: document.querySelector('.input'),
-    btn: document.querySelector('.btn-del')
+    main: document.querySelector('.js-search'),
+    input: document.querySelector('.input-text'),
+    btn: document.querySelector('.btn-del'),
 }
+
+let counter = 0;
+
+
 ref.btn.addEventListener('click', onDel)
-
-
-ref.input.addEventListener('input', _.debounce(onInput, 500)
-)
-   
+ref.input.addEventListener('input', _.debounce(onInput, 1000))
 
 
 function onInput(event)
 {
+    let name = event.target.value.trim()
+   
     
-    console.log(event.target.value)
-    let name = event.target.value
-    
-    fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(r => {
-    return r.json()
-}).then(country => {
-    ref.main.insertAdjacentHTML('beforeend', murkup(country))
-    console.log(country)
-    
-})
+    if(name.length === 0)
+    {
+        clear();
+        return;
+      
+    }
+    else {
+        counter += 1;
+        if (counter === 1)
+        {
+            fetch(`${mainHref}/v2/name/${name}`).then(response => {
+                return response.json()
+            }).then(country => {
+                render(country)
+            }).catch(clear)
+        }
+        return
+    }
+        
 }
+
+
 
 
 function onDel(){
-    location.reload()
+   clear()
+   }
+
+
+
+function render(country) {
+
+  if (country.length === 1)
+    {
+        ref.main.insertAdjacentHTML('beforeend', markup(country))
+        counter += 1
+        
+        return
+    
+        }
+   
+
+     if (country.length <= 10 && country.length > 1) {
+         ref.main.insertAdjacentHTML('beforeend', remarkup(country))
+        console.log(country)
+       return
+    }
+    
+    
+    
+      if (country.length > 10) {
+            error({
+                text: 'to many country',
+                type: 'info',
+           
+            })
+         return
+      }
+    
+      else {
+           error({
+                text: 'Sorry, we can not find your country, please try again',
+                type: 'info',
+           
+            })
+         return
+          
+      }
+   
+    
+  
+}
+ 
+
+
+function clear()
+{
+    ref.main.innerHTML = "";
+    counter = 0;
+    ref.input.value = "";
+   
 }
 
 
 
- 
+
+
+
